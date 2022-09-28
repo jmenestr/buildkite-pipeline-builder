de30a7e796cb849362e06076602a253bf88cc265
Justin Menestrina <jmenestr@gmail.com>
2022-09-28 09:51:13 -0600

    add wait step

---
+++ src/steps/wait/index.ts
@@ -0,0 +1,33 @@
+import { BaseStep, StepShape } from "../base/BaseStep";
+import { JSONSerialization } from "../base/serialization";
+
+type WaitStepShape = StepShape<{
+    wait: "~";
+    if?: string;
+    continue_on_failure?: boolean;
+}>
+
+export class WaitStep extends BaseStep implements JSONSerialization<WaitStepShape> {
+    private readonly _wait = '~' as const;
+    private _if?: string;
+    private _continue_on_failure?: boolean;
+
+    if(condition: string) {
+        this._if = condition;
+        return this;
+    }
+
+    continueOnFailure(cof: boolean) {
+        this._continue_on_failure = cof;
+        return this;
+    }
+
+    toJSON() {
+        return BaseStep.pruneJson({
+            ...super.toJSON(),
+            wait: this._wait,
+            if: this._if,
+            continue_on_failure: this._continue_on_failure
+        })
+    }
+}
---
+++ src/steps/wait/wait.test.ts
@@ -0,0 +1,44 @@
+import test from "ava";
+import { WaitStep } from ".";
+
+test('empty wait step only returns ~', t => {
+    const wait = new WaitStep();
+    t.like(wait.toJSON(),
+        {
+            wait: "~"
+        }
+    )
+});
+
+
+test('can correctly add a if condition', t => {
+    const wait = new WaitStep();
+
+    wait.if('commit == test')
+    t.like(wait.toJSON(),
+        {
+            wait: "~",
+            if: 'commit == test'
+        }
+    )
+})
+
+test('can correctly add a continueOnFailure boolean', t => {
+    const wait = new WaitStep();
+
+    wait.continueOnFailure(true)
+    t.like(wait.toJSON(),
+        {
+            wait: "~",
+            continue_on_failure: true
+        }
+    )
+
+    wait.continueOnFailure(false)
+    t.like(wait.toJSON(),
+        {
+            wait: "~",
+            continue_on_failure: false
+        }
+    )
+})
